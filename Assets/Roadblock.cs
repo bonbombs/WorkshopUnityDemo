402a26766ee372af304edec7b577376166bafaea
bonbombs <kelly.zhang2306@gmail.com>
2017-09-10 13:48:29 -0400

    Score screen

--- Assets/Roadblock.cs
+++ Assets/Roadblock.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Roadblock : MonoBehaviour {
+    
+    public Text scoreText;
+    public int scoreIncrease = 1;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -10,6 +14,11 @@ public class Roadblock : MonoBehaviour {
         {
             // Set the player's position back to defined position
             collision.gameObject.transform.position = new Vector3(0f, 1, 6f);
+
+            // Let's update enemy score everytime they block the player
+            int score = int.Parse(scoreText.text);  // Get current score from UI
+            score += scoreIncrease;                 // increment score
+            scoreText.text = score.ToString();      // Update new score onto the UI
         }
     }
 }
--- Assets/ShootAtTarget.cs
+++ Assets/ShootAtTarget.cs
@@ -21,6 +21,15 @@ public class ShootAtTarget : MonoBehaviour {
         BulletBehavior bulletInstance = Instantiate(bullet, transform.position, Quaternion.identity);
         // Point the bullet towards the target also
         bulletInstance.transform.forward = transform.forward;
+        Roadblock roadblockComp = bulletInstance.GetComponent<Roadblock>();
+        // If our bullet has a Roadblock attached,
+        if (roadblockComp != null)
+        {
+            // Set our roadblock score reference to theirs
+            // NOTE:    Prefabs are unable to store references from the scene pre-play
+            //          We're going to have to tell it what the reference is
+            roadblockComp.scoreText = GetComponent<Roadblock>().scoreText;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
